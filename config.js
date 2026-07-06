babc47aba9b642d262ca06a788173774c8533e0d
Emmanuel Nwani <emmanuelnwani2008@gmail.com>
2026-07-06 04:32:31 -0700

    Fixed API_URL

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 // config.js
 (function() {
     // If Cloudflare Pages sets an environment variable, use it
-    const apiUrl = window.__API_URL__ || 
-                   (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ? 
-                   'http://localhost:3000/api' : 
-                   'https://paypoint-backend-9m63.onrender.com/api');
-    window.__API_URL__ = apiUrl;
-    console.log('[Config] API URL:', apiUrl);
-})();
+    const apiUrl = window.__API_URL__ || 'https://paypoint-backend-9m63.onrender.com/api';
